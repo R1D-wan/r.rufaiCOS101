11cf62ead4a360462156c21bb067fbf73e7efeb7
ridwan rufai <ridwan.rufai@pau.edu.ng>
2026-08-31 20:49:00 +0100

    added changes

---
+++ week-2/practice_1.rs
@@ -0,0 +1,4 @@
+fn main () {
+println!("welcome to cos 101");
+
+}
---
+++ week-2/practice_2.rs
@@ -0,0 +1,8 @@
+fn main(){
+	println!(); // prints just a
+	println!("hello"); //prints hello
+	println!("formats{} arguements", "some"); //prints format some arguements
+	println!("My first is {}. i am a student of cos 101 ","ridwan rufai" );
+
+
+}
