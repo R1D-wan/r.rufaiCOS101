8b52fd187ad429094ac1b926bf9dafd69606fe10
ridwan rufai <ridwan.rufai@pau.edu.ng>
2026-09-04 01:24:11 +0100

    my second week projects

---
+++ week-2/projects/project_3.rs
@@ -0,0 +1,8 @@
+fn main (){
+	let p:f64 = 210000.00;
+	let r:f64 = 5.0;
+	let n:f64 = 3.0;
+	let a:f64 = p * ( 1.0 - (r/100.0)).powf(n);
+	println!("The value of tv after three years is {}", a );
+
+}
